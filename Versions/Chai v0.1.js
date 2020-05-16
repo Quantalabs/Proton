acae79c1713327790218ea668e1933fabcfe4f72
Q Labs <55121845+Quantalabs@users.noreply.github.com>
2020-05-16 15:50:30 -0700

    Update Chai v0.1.js

--- Versions/Chai v0.1.js
+++ Versions/Chai v0.1.js
@@ -1,20 +1,32 @@
-var create = function (element,text,src,id,name) {
-  var a = document.createElement(element);
-  document.body.appendChild(a);
-  a.textContent = text;
-  a.src = src;
-  a.id = id;
-  a.name = name;
-};
-var style = function (PrpName, PrpValue, Id) {
-  var b = document.getElementById(id);
-  b.style.PrpName = PrpValue;
-};
-var addText = function (text, id) {
-  var c = document.getElementById(id);
-  c.textContent += text;
-};
-var activate = function (eventName,callFunction,name1) {
-  var clickerButton = document.getElementById(name1);
-  clickerButton.addEventListener(eventName,callFunction);
-};
+    var create = function (element,text,src,id,class1) {
+      var a = document.createElement(element)
+      document.body.appendChild(a)
+      a.textContent = text
+      a.src = src
+      a.id = id
+      a.class = class1
+    }
+    var style = function (property,value,id) {
+        document.getElementById(id).style[property] = value
+    }
+    var addText = function (text, id) {
+      var c = document.getElementById(id)
+      c.textContent += text
+    }
+    var activate = function (eventName,callFunction,name1) {
+      var clickerButton = document.getElementById(name1)
+      clickerButton.addEventListener(eventName,callFunction)
+    }
+    var changeText = function (text,id) {
+      var a = document.getElementById(id)
+      a.textContent = text;
+    }
+    var addIn = function (element, text, src, id, class1, appendId) {
+        var a = document.createElement(element)
+        var b = document.createTextNode(text)
+        a.appendChild(b)
+        document.getElementById(appendId).appendChild(a)
+        a.src = src
+        a.id = id
+        a.class = class1
+    }
